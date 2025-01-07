0385265cfbbb2c62f534da4010184f706621e989
Rishabh0815 <0845rishabh@gmail.com>
2025-01-07 06:39:00 +0000

    Refactor application to replace employee management with movie management  - Updated `vercel.json` to reflect changes - Modified `controllers/index.js` to handle movie data instead of employees - Added new file `BD6.6HW1/index.js` for movie-related logic - Added `jest.config.js` to configure Jest for testing - Introduced `server.js` to set up server - Created test file `app1.test.js` to test movie API endpoints  All references to employees have been replaced with movies in the codebase.

---
+++ BD6.6HW1/index.js
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const express = require("express");
+const { getAllMovies, getMovieById } = require("./controllers");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/movies", async (req, res) => {
+  const movies = await getAllMovies();
+  res.json({ movies });
+});
+
+app.get("/movies/details/:id", async (req, res) => {
+  let movie = await getMovieById(parseInt(req.params.id));
+  res.json({
+    movie,
+  });
+});
+
+module.exports = { app };
