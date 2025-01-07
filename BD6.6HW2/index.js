b8741e5fd1f3bb69101262c873c421d43dc0ae9f
Rishabh0815 <0845rishabh@gmail.com>
2025-01-07 07:03:25 +0000

    Refactor application to replace employee management with game management  - Updated `vercel.json` to reflect changes - Modified `controllers/index.js` to handle game data instead of employees - Added new file `BD6.6HW2/index.js` for game-related logic - Added `jest.config.js` to configure Jest for testing - Introduced `server.js` to set up server - Created test file `app2.test.js` to test game API endpoints  All references to employees have been replaced with games in the codebase.

---
+++ BD6.6HW2/index.js
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const express = require("express");
+const { getAllGames, getGameById } = require("./controllers");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/games", async (req, res) => {
+  const games = await getAllGames();
+  res.json({ games });
+});
+
+app.get("/games/details/:id", async (req, res) => {
+  let game = await getGameById(parseInt(req.params.id));
+  res.json({
+    game,
+  });
+});
+
+module.exports = { app };
